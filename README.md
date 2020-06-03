18d7594417116461c51e7385e9910c6149d9095d
apenas-ana <ana.sevlaraujo@gmail.com>
2020-06-03 19:44:47 -0300

    limpando a estrutura da aplicação web - react

--- README.md
+++
@@ -1,7 +0,0 @@
-# Ecoleta
-
-O Ecoleta é uma aplicação web e mobile criada com o intuito de conectar empresas e entidades de coleta de resíduos com pessoas que desejam descartar resíduos corretamente.
-
-Esta aplicação faz parte do conteúdo prático do evento online da Rockeatseat, o NLW (Next Level Week), do qual participei. O NLW é uma semana prática com muito código, desafios e networking.
-
-O back-end será desenvolvido com Node.js e o front-end com React (web) e React Native (mobile).
--- server/src/server.ts
+++ server/src/server.ts
@@ -13,13 +13,14 @@ import express from 'express'; // essa biblioteca tem apenas o código, não tem
 // instalar biblioteca de definição de tipos com npm install @types/express, mas apenas para dev (-D), pois em produção não será necessário pois o código já terá sido convertido para JavaScript.
 import routes from './routes';
 import path from 'path';
+import cors from 'cors';
 
 const app = express();
 
 app.use(express.json()); // forma do express entender o corpo da nossa requisição em formato json
+app.use(cors());
 app.use(routes);
 
 app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads'))); // express.static -> servir arquivos estáticos de uma pasta específica    
 
 app.listen(3333);
-
