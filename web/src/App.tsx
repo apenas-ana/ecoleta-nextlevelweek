0aacf0302623da1b70561b4304c0facd828c3c69
apenas-ana <ana.sevlaraujo@gmail.com>
2020-06-06 00:22:38 -0300

    cria estrutura hmtl da página de cadastro do ponto de coleta

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,25 +1,16 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './App.css';
+import Routes from './routes';
 
 // JSX: Sintaxe de XML dentro do JavaScript
 
-import Header from './Header';
-
 function App() {
-  const [counter, setCounter] = useState(0); // [valor do estado, função pra atualizar o valor do estado]
-
-  function handleButtonClick() {
-    setCounter(counter + 1);
-  }
-
   return (
-    <div>
-      <Header title="Hello World"/>
-
-      <h1>{counter}</h1>
-      <button type="button" onClick={handleButtonClick}>Aumentar</button>
-    </div>
+    <Routes />
   );
 }
 
 export default App;
+
+
+// npm install react-router-dom
